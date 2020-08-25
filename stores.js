2965197700c8c56286ff2f6f20d251cd46bdf429
Robert Pop <63066180+robertpop47@users.noreply.github.com>
2020-08-25 21:18:26 +0300

    Delete stores.js

--- stores.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import battleship from "./reducers";
-
-const store = createStore(
-  battleship,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-export default store;
